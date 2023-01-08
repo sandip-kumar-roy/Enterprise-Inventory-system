998cfcdd15a78af7b0b31e9ee268189676736948
Sandip-Kumar-Roy <101457128+sandip-kumar-roy@users.noreply.github.com>
2023-01-09 00:32:08 +0530

    Add files via upload

---
+++ src/main/java/com/sandip123/ApplicationInitializer.java
@@ -0,0 +1,32 @@
+package com.tirmizee;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class ApplicationInitializer extends SpringBootServletInitializer implements CommandLineRunner {
+
+	public final Logger LOGGER = LoggerFactory.getLogger(getClass());
+	
+	public static void main(String...args) throws MalformedURLException, IOException {
+		SpringApplication.run(ApplicationInitializer.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+	
+	/*@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ApplicationInitializer.class);
+    }*/
+	
+} 
